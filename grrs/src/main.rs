b85f99bb9a7a8982f4472e3a086071499fc1442c
Paulo Vital <paulo.vital@ibm.com>
2024-08-20 10:32:25 +0200

    feat: Grep similar first version.
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

---
+++ grrs/src/main.rs
@@ -0,0 +1,18 @@
+// (c) Copyright Paulo Vital 2024
+
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no path given");
+
+    let args = Cli {
+        pattern,
+        path: std::path::PathBuf::from(path),
+    };
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+}
