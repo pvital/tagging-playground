1613d821e51cfb884cbb1dab611506ddd4de551a
Paulo Vital <paulo.vital@ibm.com>
2024-08-20 10:50:48 +0200

    feat: add Clap support.
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,18 +1,18 @@
 // (c) Copyright Paulo Vital 2024
 
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
+    /// The path to the file to read
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
 
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
